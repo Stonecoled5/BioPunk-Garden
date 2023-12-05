a229465c3b9ceae530d47be6bc483cf8f0c474d6
Cole Johnstone <colejohnstone25@yahoo.com>
2023-12-05 13:58:21 -0600

    added descriptions

--- descriptions/good_plant.lua
+++ descriptions/good_plant.lua
@@ -1,4 +1,5 @@
 local struct = {}
-struct.info = "This is a good plant"
+struct.info = "This is a good plant. The goal of the game is to have good plants throughout your entire garden. These plants have a 50/50 chance of spawning from regular dirt and 100% chance spawning from fertilized dirt."
 struct.pic = hash("good-plant-placeholder")
 return struct
+--If you have a plot full of all good plants you get an extra pesticide permanantly. If you have a plot with at least one good plant and no bad plants you get an extra steroids permanently. These are unrepeatable for each plot.
