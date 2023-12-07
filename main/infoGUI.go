ea54c228d0c926eb5ce160d0a8f9584ed8ad59c6
Cole Johnstone <colejohnstone25@yahoo.com>
2023-12-07 11:26:05 -0600

    fixed some sound bugs

--- main/infoGUI.go
+++ main/infoGUI.go
@@ -13,3 +13,72 @@ components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "negativeClick"
+  type: "sound"
+  data: "sound: \"/assets/audio/negativeClick.wav\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "neutralClick"
+  type: "sound"
+  data: "sound: \"/assets/audio/neutralClick.wav\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "positiveClick"
+  type: "sound"
+  data: "sound: \"/assets/audio/positiveClick.wav\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
